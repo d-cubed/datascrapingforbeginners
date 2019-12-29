72ca862ecba24e5aa78a3009a77ab28953ee546f
Don Davis <ddavis2@neisd.net>
2019-12-29 12:16:53 -0600

    A nice little text scraper based on pdfplumber Sun Dec 29 12:16:53 CST 2019

---
+++ getPDFtext.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+import pdfplumber
+import sys
+
+print ("You are currently running %s" % (sys.argv[0]))
+
+if len(sys.argv) < 2:
+    print("You should supply the name of a pdf to extract the text from!")
+    sys.exit()
+else:
+    pdf = pdfplumber.open(sys.argv[1])
+
+print(str(sys.argv[1]) + " has %s pages. " % str(len(pdf.pages)))
+
+#we use this to eyeball the boundingbox
+for p in range(1,2):
+    tp = pdf.pages[p]
+    print("The width of page " + str(p) + " is " + str(tp.width))
+    print("The height of page " + str(p) + " is " + str(tp.height))
+
+#Note it's expecting a tuple i.e. () in ()
+#(x0, top, x1, bottom)
+#this range is just to trim off watermark text - ymmv
+## wrt .encode() 50K causes and solutions to this problem
+filename = str(sys.argv[1]+".txt")
+
+with open(filename, "a") as textdump:
+    for p in range (0,len(pdf.pages)):
+        tp = pdf.pages[p]
+        thetext = tp.within_bbox((20,20,540,660)).extract_text().encode('utf-8')
+        print(thetext)
+        textdump.write(thetext)
